fee1467273bb9596a18a8f8c02ac57d73b043172
AngelPerezCoder2020 <angelperez.cmp.b.16@gmail.com>
2023-03-02 18:10:59 -0600

    Functional Interface added

---
+++ src/Limpiador.java
@@ -0,0 +1,4 @@
+@FunctionalInterface
+public interface Limpiador {
+    String limpiar(String str);
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println(limpiador("a    ,n.....ge,,,,, l"));
+    }
+    private static void hilos(){
         Thread hilo1 = new Thread(new AlfanumericoTarea(Tipo.NUMERO));
         Thread hilo2 = new Thread(new AlfanumericoTarea(Tipo.LETRA));
         Thread hilo3 = new Thread(()->{
@@ -9,5 +12,15 @@ public class Main {
         hilo1.start();
         hilo3.start();
         hilo2.start();
+    }
+    private static String limpiador(String str){
+        Limpiador l = Main::clean;
+        return l.limpiar(str);
+    }
+    public static String clean(String str){
+        String result = str.replace(",","")
+                .replace(" ","")
+                .replace(".","");
+        return result.toUpperCase();
     }
 }
