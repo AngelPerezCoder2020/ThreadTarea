171d0905d9001eac0febd5b9377d2dd37eb2b7ee
AngelPerezCoder2020 <angelperez.cmp.b.16@gmail.com>
2023-03-02 20:21:53 -0600

    another functional interface

---
+++ src/CantVecesPalabra.java
@@ -0,0 +1,5 @@
+import java.util.Map;
+
+public interface CantVecesPalabra {
+    Map<String, Integer> procesar(String str);
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(limpiador("a    ,n.....ge,,,,, l"));
+        String frase = "Esta es la frase frase frase frase que va procesar";
+        //System.out.println(limpiador("a    ,n.....ge,,,,, l"));
+        CantVecesPalabra cvp = Main::procesar;
+        System.out.println(cvp.procesar(frase));
     }
     private static void hilos(){
         Thread hilo1 = new Thread(new AlfanumericoTarea(Tipo.NUMERO));
@@ -22,5 +29,30 @@ public class Main {
                 .replace(" ","")
                 .replace(".","");
         return result.toUpperCase();
+    }
+    public static Map<String,Integer> procesar(String str){
+        String masRepetida = "";
+        String sigPalabra = "";
+        Integer cant = 0;
+        Integer max = 0;
+        Map<String,Integer> palMap = new HashMap<>();
+        Map<String,Integer> result = new HashMap<>();
+        String[] palabras = str.toLowerCase().split(" ");
+        for(String p:palabras){
+            if(palMap.containsKey(p)) palMap.put(p,palMap.get(p)+1);
+            else palMap.put(p,1);
+        }
+        System.out.println(palMap);
+        Iterator<String> llaves = palMap.keySet().iterator();
+        while(llaves.hasNext()){
+            sigPalabra = llaves.next();
+            cant = palMap.get(sigPalabra);
+            if(cant>max){
+                max = cant;
+                masRepetida = sigPalabra;
+            }
+        }
+        result.put(masRepetida,max);
+        return result;
     }
 }
